196f18e044b8a487cbd463ac7353ae4a58f9c3a0
VladyslavMazurets <vladmazurec@gmail.com>
2023-05-07 17:16:21 +0200

    adaptive Home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,7 +9,8 @@ function Home() {
   return (
     <div
       className="bg-[url('/src/assets/imgs/stars-bg.jpg')] bg-center
-    bg-no-repeat bg-cover h-[84vh] w-full flex justify-center items-center"
+    bg-no-repeat bg-cover h-[84.2vh] w-full flex justify-center items-center
+    min-[360px]:h-[87.8vh] md:h-[89vh]"
     >
       <Link
         to="/pages/1"
@@ -19,11 +20,12 @@ function Home() {
         <img
           src={Img}
           alt="Characters-Img"
-          className="w-[600px] h-[350px] object-cover"
+          className="w-[230px] h-[200px] object-cover min-[360px]:w-[300px]
+          min-[360px]:h-[250px] md:w-[600px] md:h-[350px]"
         />
         <span
-          className="text-4xl font-raleway font-semibold 
-        py-4"
+          className="text-xl font-raleway font-semibold 
+        py-4 md:text-4xl"
         >
           Star Wars Characters
         </span>
